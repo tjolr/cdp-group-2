f538bb19f28b4b42dc7c282573759487e017f9f3
Tjøl Ravndal <tjol.ravndal@gmail.com>
2021-09-08 13:19:03 +0200

    feat-#27: set up redux toolkit with userSlice

--- game/kegeland/App.tsx
+++ game/kegeland/App.tsx
@@ -1,35 +1,30 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { Provider as StoreProvider } from 'react-redux';
 import GameScreen from './src/screens/Game/GameScreen';
 import GameOverScreen from './src/screens/GameOver/GameOverScreen';
 import HomeScreen from './src/screens/Home/HomeScreen';
 import LoginScreen from './src/screens/Login/LoginScreen';
 import MainMenuScreen from './src/screens/MainMenu/MainMenuScreen';
+import { store } from './state-management/store';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="MainMenu" component={MainMenuScreen} />
-        <Stack.Screen name="Game" component={GameScreen} />
-        <Stack.Screen name="GameOver" component={GameOverScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <StoreProvider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="MainMenu" component={MainMenuScreen} />
+          <Stack.Screen name="Game" component={GameScreen} />
+          <Stack.Screen name="GameOver" component={GameOverScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </StoreProvider>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 export default App;
---
+++ game/kegeland/state-management/user/userSlice.types.ts
@@ -0,0 +1,7 @@
+export interface User {
+  userId: string;
+  email: string;
+  displayName: string;
+  firstName: string;
+  lastName: string;
+}

293a3745fddd4f03fd5f5aa19ed1150be3f78d3a
Sondre Dahl <sondreo.dahl@gmail.com>
2021-09-08 09:11:06 +0200

    feat-#19: Remove unused dotenv require statement

--- game/kegeland/App.tsx
+++ game/kegeland/App.tsx
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-require('dotenv').config();
 
 export default function App() {
   return (

8eea91818a21abd66c4b18c2a11aa0c249e56047
Sondre Dahl <sondreo.dahl@gmail.com>
2021-09-08 13:07:27 +0200

    feat-#18: Add google authentication methods to back-end
    * feat-#18: Add a package for env configuration
    
    * feat-#18: Create an API namespace and add authenticatiion methods

--- game/kegeland/babel.config.js
+++ game/kegeland/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', ['module:react-native-dotenv']],
+    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
+    plugins: ['module:react-native-dotenv'],
   };
 };

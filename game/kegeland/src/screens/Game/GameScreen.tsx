eabdd15c85a6f480be3dc9259f8b44672040e149
Tjøl Ravndal <tjol.ravndal@gmail.com>
2021-09-08 11:51:21 +0200

    chore: add prettier and prettier config settings

--- game/kegeland/src/screens/Game/GameScreen.tsx
+++ game/kegeland/src/screens/Game/GameScreen.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { View, Text, Button } from "react-native";
-import { StyleSheet } from "react-native";
-import { NavigationScreenProps } from "../navigation.types";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { NavigationScreenProps } from '../navigation.types';
 
 const GameScreen = ({ navigation }: NavigationScreenProps) => {
-  const handleGameOverPress = () => navigation.navigate("GameOver");
+  const handleGameOverPress = () => navigation.navigate('GameOver');
 
   return (
     <View style={styles.container}>
@@ -19,9 +19,9 @@ const GameScreen = ({ navigation }: NavigationScreenProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 

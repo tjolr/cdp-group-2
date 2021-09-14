b99c5d34869ba24360be21a6d4ab9775ef0de29f
Tjøl Ravndal <tjol.ravndal@gmail.com>
2021-09-14 16:56:44 +0200

    feat-#5: Style login screen

--- game/kegeland/src/screens/Login/LoginScreen.tsx
+++ game/kegeland/src/screens/Login/LoginScreen.tsx
@@ -1,23 +1,43 @@
+import { Box, Button, Center, Heading, Stack, Text } from 'native-base';
 import React from 'react';
-import { View, Text, Button } from 'react-native';
 import { StyleSheet } from 'react-native';
 import { NavigationScreenProps } from '../navigation.types';
 
 const LoginScreen = ({ navigation }: NavigationScreenProps) => {
-  const handleLoginPress = () => navigation.navigate('MainMenu');
+  const handleLoginPress = async () => {
+    navigation.navigate('MainMenu');
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>Login Screen</Text>
-      <Button onPress={handleLoginPress} title="Login" />
-    </View>
+    <Box
+      bg={{
+        linearGradient: {
+          colors: ['rose.300', 'pink.200'],
+          start: [0, 1],
+          end: [1, 0],
+        },
+      }}
+      style={styles.container}
+    >
+      <Center>
+        <Stack direction="column" space={'lg'}>
+          <Heading>Log in</Heading>
+          <Text fontSize="lg">Please log in using your Google account</Text>
+
+          <Center>
+            <Button colorScheme="teal" size="lg" onPress={handleLoginPress}>
+              Google login
+            </Button>
+          </Center>
+        </Stack>
+      </Center>
+    </Box>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },

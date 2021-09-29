bda2b5d781b729f70fdbee603166920185971239
Tommy Woldseth <tommyew7@gmail.com>
2021-09-29 11:38:26 +0200

    feat-#67: Make breakable wall obstacle

--- game/kegeland/utils/random.ts
+++ game/kegeland/utils/random.ts
@@ -3,27 +3,24 @@ import { Dimensions } from 'react-native';
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 
-interface IRandom {
-  min: number;
-  max: number;
-}
-
-export const getRandom = (props: IRandom) => {
-  return Math.floor(Math.random() * (props.max - props.min + 1) + props.min);
+export const getRandom = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 export const getPipeSizePos = (addToPosX = 0) => {
   let min = 300;
   let max = windowHeight - 100;
-  let yPosTop = -getRandom({ min, max });
+  let yPosTop = -getRandom(min, max);
   let pipe;
   let yCoord;
 
-  // Randomly decide if the pipe should be at the bottom or top
-  const rndInt = Math.floor(Math.random() * 2) + 1;
+  // Randomly decide if the pipe should be at the bottom, top or full height wall
+  const rndInt = getRandom(1, 3);
   if (rndInt == 1) {
     yCoord = yPosTop;
-  } else {
+  } else if (rndInt == 2) {
     yCoord = windowHeight * 2 + 200 + yPosTop;
+  } else {
+    yCoord = 0;
   }
   pipe = {
     pos: { x: windowWidth + addToPosX, y: yCoord },

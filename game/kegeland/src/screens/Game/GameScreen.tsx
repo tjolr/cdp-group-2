39b9552045aaf305673779c2005918f7b17d3d7e
Tjøl Ravndal <tjol.ravndal@gmail.com>
2021-10-12 17:13:33 +0200

    feat-#106: show lives as hearts

--- game/kegeland/src/screens/Game/GameScreen.tsx
+++ game/kegeland/src/screens/Game/GameScreen.tsx
@@ -1,4 +1,4 @@
-import { Text } from 'native-base';
+import { Box, HStack, Text } from 'native-base';
 import { ImageBackground, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { GameEngine } from 'react-native-game-engine';
@@ -22,6 +22,10 @@ import {
 import { NavigationScreenProps } from '../navigation.types';
 import Background from '../../../assets/hills.png';
 import UnderWaterBackground from '../../../assets/underwater-background.png';
+import { AntDesign } from '@expo/vector-icons';
+import { theme } from '../../styles/theme';
+import { Foundation } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 const GameScreen = ({ route, navigation }: NavigationScreenProps) => {
   const params = route.params;
@@ -52,38 +56,69 @@ const GameScreen = ({ route, navigation }: NavigationScreenProps) => {
 
   return (
     <ImageBackground source={backgroundImage} style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 25,
-            fontWeight: 'bold',
-            top: 45,
-          }}
+      <Box style={{ zIndex: 100 }}>
+        <HStack
+          space={3}
+          alignItems="center"
+          justifyContent="space-between"
+          m={4}
         >
-          Lives: {lives}
-        </Text>
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 25,
-            fontWeight: 'bold',
-            top: 50,
-          }}
-        >
-          Points: {points}
-        </Text>
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 25,
-            fontWeight: 'bold',
-            top: 50,
-          }}
-        >
-          Speed: {obstacleSpeed}
-        </Text>
-      </View>
+          <HStack alignItems="center" space={1}>
+            <Foundation
+              name="trophy"
+              size={30}
+              color={theme.colors.yellow[600]}
+            />
+            <Text
+              style={{
+                fontSize: 30,
+                fontWeight: 'bold',
+                color: theme.colors.yellow[600],
+              }}
+              ml={1}
+            >
+              {points}
+            </Text>
+          </HStack>
+
+          <HStack alignItems="center" space={1}>
+            <Ionicons
+              name="speedometer"
+              size={30}
+              color={theme.colors.darkBlue[700]}
+            />
+
+            <Text
+              style={{
+                fontSize: 30,
+                fontWeight: 'bold',
+                color: theme.colors.darkBlue[700],
+              }}
+              ml={1}
+            >
+              {obstacleSpeed}
+            </Text>
+          </HStack>
+
+          <HStack
+            alignItems="center"
+            space={1}
+            width={35}
+            justifyContent="flex-end"
+          >
+            {Array(lives)
+              .fill(0)
+              .map((_, index: number) => (
+                <AntDesign
+                  key={index}
+                  name="heart"
+                  size={30}
+                  color={theme.colors.red[500]}
+                />
+              ))}
+          </HStack>
+        </HStack>
+      </Box>
       <GameEngine
         entities={entities()}
         style={{

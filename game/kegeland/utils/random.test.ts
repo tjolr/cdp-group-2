ec340c30f101d9b8489dcd7eff032f28aac1ab75
Tommy Woldseth <tommyew7@gmail.com>
2021-10-19 12:44:00 +0200

    chore-#95: Add unit tests

---
+++ game/kegeland/utils/random.test.ts
@@ -0,0 +1,12 @@
+import { getPipeSizePosBottom } from './random';
+
+describe('Testing random utility functions', () => {
+  it('Test the width of the obstacle', () => {
+    const pipeSizePos = getPipeSizePosBottom();
+    expect(pipeSizePos.pipe.size.width).toBe(75);
+  });
+  it('Test y-position of obstacle', () => {
+    const pipeSizePos = getPipeSizePosBottom();
+    expect(pipeSizePos.pipe.pos.y).toBeGreaterThan(399);
+  });
+});

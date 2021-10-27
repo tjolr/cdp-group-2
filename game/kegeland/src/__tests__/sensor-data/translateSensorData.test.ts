86d4379a7c151518df2964bf75bf35e3132509e1
Tommy Woldseth <tommyew7@gmail.com>
2021-10-27 09:28:24 +0200

    Updated tests and used default case

--- game/kegeland/src/__tests__/sensor-data/translateSensorData.test.ts
+++ game/kegeland/src/__tests__/sensor-data/translateSensorData.test.ts
@@ -2,12 +2,28 @@ import { translateSensorData } from '../../../utils/translateSensorData';
 import { GameMode } from '../../../state-management/game/gameMode';
 
 describe('Testing translation from sensor data to velocity', () => {
-  it('Test the width of the obstacle', () => {
+  it('Test velocity for one control game mode', () => {
     const testData = [750, 752, 750, 754, 750, 756, 752, 751, 37];
     expect(translateSensorData(testData, GameMode.OneControl)).toBe(-3);
   });
-  it('Test the width of the obstacle', () => {
-    const testData = [752, 752, 752, 752, 752, 752, 752, 752, 37];
+  it('Test break wall action', () => {
+    const testData = [752, 752, 751, 752, 752, 752, 752, 752];
     expect(translateSensorData(testData, GameMode.MultiControl)).toBe(1);
+  });
+  it('Test flying up action', () => {
+    const testData = [756, 755, 756, 755, 750, 750, 750, 750];
+    expect(translateSensorData(testData, GameMode.MultiControl)).toBe(-4);
+  });
+  it('Test flying down action', () => {
+    const testData = [750, 750, 750, 750, 756, 755, 756, 755];
+    expect(translateSensorData(testData, GameMode.MultiControl)).toBe(4);
+  });
+  it('Test flying down slowly', () => {
+    const testData = [745, 745, 745, 745, 749, 749, 749, 749];
+    expect(translateSensorData(testData, GameMode.MultiControl)).toBe(2);
+  });
+  it('Test default case', () => {
+    const testData = [740, 740, 740, 740, 740, 740, 740, 740];
+    expect(translateSensorData(testData, GameMode.MultiControl)).toBe(0);
   });
 });
--- game/kegeland/utils/translateSensorData.ts
+++ game/kegeland/utils/translateSensorData.ts
@@ -5,7 +5,7 @@ export enum Actions {
   MEDIUM = 3,
   LOW = 2,
   BREAK_WALL = 1,
-  DEFAULT = -1,
+  DEFAULT = 0,
 }
 
 export const translateSensorData = (
@@ -13,22 +13,23 @@ export const translateSensorData = (
   gameMode: GameMode
 ) => {
   const pressureData = data.slice(0, 8);
-  const averagePressure =
-    pressureData.reduce((a, b) => a + b) / pressureData.length;
   return gameMode === GameMode.OneControl
-    ? singleControlTranslation(averagePressure)
+    ? singleControlTranslation(pressureData)
     : multipleControlTranslation(pressureData);
 };
 
-const singleControlTranslation = (averagePressure: number) => {
+const singleControlTranslation = (pressureData: Array<number>) => {
+  const averagePressure =
+    pressureData.reduce((a, b) => a + b) / pressureData.length;
   return -checkPressure(averagePressure);
 };
 
 const multipleControlTranslation = (pressureData: Array<number>) => {
-  const treshold = 1;
+  const treshold = 2;
   const upperSensors = pressureData.slice(0, 4);
   const lowerSensors = pressureData.slice(4, 8);
   const allSensors = pressureData.slice(0, 8);
+  const averageTotal = allSensors.reduce((a, b) => a + b) / allSensors.length;
   const averageUpperPressure =
     upperSensors.reduce((a, b) => a + b) / upperSensors.length;
   const averageLowerPressure =
@@ -36,12 +37,13 @@ const multipleControlTranslation = (pressureData: Array<number>) => {
   if (averageUpperPressure - treshold > averageLowerPressure) {
     return -checkPressure(averageUpperPressure);
   } else if (averageLowerPressure - treshold > averageUpperPressure) {
-    return checkPressure(averageUpperPressure);
-  } else {
+    return checkPressure(averageLowerPressure);
+  } else if (averageTotal > 750) {
     return Actions.BREAK_WALL;
-  }
+  } else return Actions.DEFAULT;
 };
 
+// Values should be based on calibration in the future
 const checkPressure = (averagePressure: number) => {
   if (averagePressure < 750) {
     return Actions.LOW;
